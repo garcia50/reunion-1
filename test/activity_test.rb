01d52787a54438170d1887fb8cfa0a5061f8bb7f
Luis Garcia <garcia@yahoo.com>
2018-01-03 09:11:42 -0700

    Test Activity has a name and participants

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -5,6 +5,25 @@ require './lib/activity'
 
 class ActivityTest < Minitest::Test
 
-  
+  def test_activity_class_exist
+    assert_instance_of Activity, Activity.new("Hiking")
+  end
+
+  def test_Activity_class_has_a_name
+    activity = Activity.new("Hiking")
+
+    assert_equal "Hiking", activity.name
+  end
+
+  def test_participants_returns_hash
+    activity = Activity.new("Hiking")
+    empty_hash = {}
+    assert_equal empty_hash, activity.participants
+  end
+
+  def method_name
+    
+  end
+
 
 end
