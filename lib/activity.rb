113bdf5d4628094ca7e2e58e6d1e032f60472738
Luis Garcia <garcia@yahoo.com>
2018-01-03 11:03:42 -0700

    add_participant

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,9 +1,27 @@
 class Activity
-  attr_reader :name, :participants
+  attr_reader :activity, :participants, :names, :bills
+  attr_accessor :total_cost
 
-  def initialize(name)
-    @name = name
-    @participants = {}
+  def initialize(activity, participants = {})
+    @activity     = activity
+    @participants = participants
+    @names        = participants[:name]
+    @bills        = participants[:cost]
+    #@total_cost   = bills.reduce(:+)
   end
 
+  def add_participant(name, bill)
+    names << name
+    bills << bill
+    @total_cost += bill
+  end
+  
+  # def split_bill
+  #   total_cost/names.count
+  # end
+
 end
+
+  
+
+
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,26 +4,54 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  attr_reader :hiking
+
+  def setup
+    @hiking = Activity.new("hiking", {name: ["rob", "greg"], cost: [20, 30]})
+
+  end
 
   def test_activity_class_exist
-    assert_instance_of Activity, Activity.new("Hiking")
+    assert_instance_of Activity, hiking
   end
 
   def test_Activity_class_has_a_name
-    activity = Activity.new("Hiking")
-
-    assert_equal "Hiking", activity.name
+    assert_equal "hiking", hiking.activity
   end
 
-  def test_participants_returns_hash
-    activity = Activity.new("Hiking")
-    empty_hash = {}
-    assert_equal empty_hash, activity.participants
+  def test_activity_returns_empty_hash
+    hike = Activity.new("hiking")
+    empty = {}
+    assert_equal empty, hike.participants
   end
 
-  def method_name
-    
+  def test_names_of_participants_are_returned
+    names = ["rob", "greg"]
+    assert_equal names, hiking.names
   end
 
+  def test_participants_has_a_name_and_an_amount
+    participant_hash = {name: "rob", cost: 20}
+    activity = Activity.new("hiking", participant_hash)
+
+    assert_equal participant_hash, activity.participants
+  end
+
+  def test_can_add_participants
+    hiking.add_participant("sal", 70)
+    assert_equal ["rob", "greg", "sal"], hiking.names
+    assert_equal [20, 30, 70], hiking.bills
+    assert_equal 120, hiking.total_cost
+  end
 
 end
+
+
+
+
+
+
+
+
+
+
